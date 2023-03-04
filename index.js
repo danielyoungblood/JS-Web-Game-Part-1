e38ef38c03aa6db5ed8398887a286d501f7103bf
daniel <danielyknechtli@gmail.com>
2023-03-04 15:21:25 -0800

    adding images double click

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 //
 //parameters contain values passed to the function when called
 //example: newImage("assets/green-character.gif","100px","100px")
-
 function newImage(imageFile, leftPOS, bottomPOS) {
   let image = document.createElement("img");
   image.src = imageFile;
@@ -13,6 +12,19 @@ function newImage(imageFile, leftPOS, bottomPOS) {
   image.style.left = leftPOS;
   image.style.bottom = bottomPOS;
   document.body.append(image);
+}
+
+function newItem(imageFile, leftPOS, bottomPOS) {
+  let sword = document.createElement("img");
+  sword.src = imageFile;
+  sword.position = "fixed";
+  sword.left = leftPOS;
+  sword.bottom = bottomPOS;
+  document.body.append(sword);
+
+  sword.addEventListener("dblclick", function () {
+    sword.remove();
+  });
 }
 
 //invoke newImage / call newImage function, passing the appropriate arguments.
@@ -28,3 +40,12 @@ newImage("assets/green-character.gif", "100px", "100px");
 //"450px"(leftPOS parameter)
 //"200px"(bottomPOS parameter)
 newImage("assets/pine-tree.png", "450px", "200px");
+
+newImage("assets/tree.png", "200px", "300px");
+newImage("assets/pillar.png", "350px", "100px");
+newImage("assets/crate.png", "150px", "200px");
+newImage("assets/well.png ", "500px", "425px");
+
+newItem("assets/sword.png", "500px", "405px");
+newItem("assets/shield.png", "165px", "185px");
+newItem("assets/staff.png", "600px", "100px");
